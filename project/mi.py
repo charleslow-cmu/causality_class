75913e5998936a5231b943ecb3d91b8d5fca18a3
Charles Low <cjlow@andrew.cmu.edu>
2020-12-07 17:12:48 +0800

    Completed mutual information testing script

--- project/mi.py
+++ project/mi.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from itertools import product
 import math
+from copy import deepcopy
 
 def dicttostr(d):
     l = []
@@ -12,10 +13,10 @@ def dicttostr(d):
 
 
 def make_column(d=1):
-    probs = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+    #probs = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
     lprobs = []
     for i in range(d):
-        p = random.choice(probs)
+        p = random.uniform(0.05, 0.95)
         lprobs.append(p)
         lprobs.append(1-p)
     return pd.Series(lprobs)
@@ -23,46 +24,60 @@ def make_column(d=1):
 
 # Given a dictionary of key and values, retrieve
 # corresponding row from df
-def lookup(var, d):
-    if isinstance(var, Variable):
-        df = var.cpd
-    else:
-        df = var
-
+def lookup(df, d):
     vset = set(df.columns)
     for k, v in d.items():
         if k in vset:
             df = df.loc[df[k] == v]
-    #print(f"Lookup of {var.name} with {dicttostr(d)} found {df.shape[0]} entries.")
+    #print(f"Lookup with {dicttostr(d)} found {df.shape[0]} entries.")
     return sum(df["p"])
 
+# Given a dictionary of key and values, 
+# Replace all matching rows in df with "p" value
+def replace(df: pd.DataFrame, d, p):
+    vset = set(df.columns)
+    l = []
+    for k, v in d.items():
+        if k in vset:
+            l.append(f"({k} == {v})")
+    idx = df.query(" & ".join(l)).index
+    df.loc[idx, "p"] *= p
 
-def make_jpd(vars):
-    vset = set()
-    for var in vars:
-        for col in var.cpd.columns:
-            if col != "p":
-                vset.add(col)
-    keys = [[0,1] for i in range(len(vset))]
-    vnames = list(vset)
-    vnames.sort()
+
+def make_jpd(jpd, parents, name):
+
+    # Insert this variable into the jpd
+    lrows = []
+    dlist = jpd.to_dict(orient="records")
+    for i in range(len(dlist)):
+        for v in [0, 1]:
+            d = deepcopy(dlist[i])
+            d[name] = v
+            lrows.append(d)
+    df = pd.DataFrame(lrows)
+    df = sort_columns(df)
+
+
+    # Make cpd from direct parents 
+    vnames = [v for v in parents] + [name]
+    vlen = len(vnames)
+    keys = [[0,1]] * vlen
     lrows = []
     for ptuple in product(*keys):
-        d = {}
-        for i, k in enumerate(ptuple):
-            d[vnames[i]] = k
+        d = {vnames[i]:v for i,v in enumerate(ptuple)}
         lrows.append(d)
-    df = pd.DataFrame(lrows)
-    df["p"] = None 
+    cpd = pd.DataFrame(lrows)
+    cpd["p"] = make_column(d=int(cpd.shape[0]/2))
+    #print(cpd)
 
-    for i in range(df.shape[0]):
-        lprobs = []
-        for var in vars:
-            row = dict(df.iloc[i, :])
-            row.pop("p")
-            lprobs.append(lookup(var, row))
-        df.loc[i, "p"] = np.prod(lprobs)
+    # Replace the jpd ps with new ones
+    dlist = cpd.to_dict(orient="records")
+    for d in dlist:
+        p = d.pop("p")
+        replace(df, d, p)
+    #print(df)
     return df
+
 
 def sort_columns(df):
     l = ["L", "X", "p"]
@@ -73,73 +88,94 @@ def sort_columns(df):
     return df.reindex(cols, axis=1)
 
 
-class Variable:
-    def __init__(self, name, causes=None):
-        if isinstance(causes, Variable):
-            causes = [causes]
-        self.name = name
-        self.causes = causes
-        self.make_cpd(name, causes)
+class Graph:
+    def __init__(self):
+        self.vars = {}
+        self.jpd = None
 
-    def make_cpd(self, name, causes):
-        if causes is None:
-            df = pd.DataFrame({name: [0,1]})
-            df["p"] = make_column(1)
-            self.cpd = df
+    def add_variable(self, name, parents=None):
+        if isinstance(parents, str):
+            parents = [parents]
+        var = Variable(name, parents)
+        #print(f"Adding {name}.")
+
+        if not parents is None:
+            valid_parents = len(set(parents)-set(self.vars.keys())) == 0
+            assert valid_parents, "Parents not found in graph!"
+
+        self.vars[name] = var
+
+        # Adding first variable
+        if len(self.vars) == 1:
+            d = {name: [0,1], "p": make_column(d=1)}
+            self.jpd = pd.DataFrame(d)
+            #print(self.jpd)
             return
 
-        jpd = make_jpd(causes)
-        lrows = []
-        for i in range(jpd.shape[0]):
-            for v in [0,1]:
-                jpd_row = dict(jpd.iloc[i,:])
-                jpd_row[name] = v
-                lrows.append(jpd_row)
-        df = pd.DataFrame(lrows)
-        df = sort_columns(df)
-        df["p"] *= make_column(d=int(df.shape[0]/2))
-        self.cpd = df
+        # Adding subsequent vars
+        self.jpd = make_jpd(self.jpd, parents, name)
+        assert math.isclose(sum(self.jpd["p"]), 1), "JPD does not sum to 1"
+        return
+
+    def calc_mi(self, X: str, Y: str, normalize=True):
+        mi = 0
+        for x in [0, 1]:
+            for y in [0, 1]:
+                Pxy = lookup(self.jpd, {X: x, Y: y})
+                #import IPython; IPython.embed(); exit(1)
+                Px = lookup(self.jpd, {X: x})
+                Py = lookup(self.jpd, {Y: y})
+                mi += Pxy * math.log(Pxy / (Px * Py))
+    
+        if normalize:
+            Hx = 0
+            for x in [0, 1]:
+                Px = lookup(self.jpd, {X: x})
+                Hx += -Px * math.log(Px)
+                #print(f"Entropy is {Hx}.")
+            mi = mi / Hx
+        print(f"MI({X};{Y}) = {mi:.10f}")
+        return mi
+    
+    def calc_condmi(self, X: str, Y: str, Z: str, normalize=True):
+        mi = 0
+        combn = [[0,1]] * 3
+        for x,y,z in product(*combn):
+            Pxyz = lookup(self.jpd, {X: x, Y: y, Z: z})
+            Pz = lookup(self.jpd, {Z: z})
+            Pxz = lookup(self.jpd, {X: x, Z: z})
+            Pyz = lookup(self.jpd, {Y: y, Z: z})
+            mi += Pxyz * math.log((Pxyz*Pz) / (Pxz * Pyz))
+
+        if normalize:
+            Hx_z = 0
+            combn = [[0,1]] * 2
+            for x,z in product(*combn):
+                Pxz = lookup(self.jpd, {X:x, Z:z})
+                Px = lookup(self.jpd, {X:x})
+                Hx_z += -Pxz * math.log(Pxz/Px)
+            mi = mi / Hx_z
+        print(f"MI({X};{Y}|{Z}) = {mi:.10f}")
+        return mi
 
 
-def calc_mi(X: Variable, Y: Variable):
-    jpd = make_jpd([X, Y])
-    mi = 0
-    for x in [0, 1]:
-        for y in [0, 1]:
-            row = jpd.loc[(jpd[X.name] == x) & (jpd[Y.name] == y), :]
-            Pxy = lookup(jpd, {X.name: x, Y.name: y})
-            Px = lookup(jpd, {X.name: x})
-            Py = lookup(jpd, {Y.name: y})
-            mi += Pxy * math.log(Pxy / (Px * Py))
-    print(mi)
-    return mi
-
-def calc_condmi(X:Variable, Y:Variable, Z:Variable):
-    jpd = make_jpd([X, Y, Z])
-    mi = 0
-    combn = [[0,1]] * 3
-    for x,y,z in product(*combn):
-        Pxyz = lookup(jpd, {X.name: x, Y.name: y, Z.name: z})
-        Pz = lookup(jpd, {Z.name: z})
-        Pxz = lookup(jpd, {X.name: x, Z.name: z})
-        Pyz = lookup(jpd, {Y.name: y, Z.name: z})
-        mi += Pxyz * math.log((Pxyz*Pz) / (Pxz * Pyz))
-    print(mi)
-    return mi
+class Variable:
+    def __init__(self, name, parents=None):
+        self.name = name
+        self.parents = parents
 
 
-L1 = Variable("L1", None)
-L2 = Variable("L2", None)
-X1 = Variable("X1", L1)
-X2 = Variable("X2", L1)
-X3 = Variable("X3", L2)
-X4 = Variable("X4", L2)
-#import IPython; IPython.embed(); exit(1)
-#mi = calc_mi(X1, X2)
-#mi = calc_mi(X1, X3)
-mi = calc_condmi(X1, X2, X3)
-mi = calc_condmi(X1, X2, X4)
-mi = calc_condmi(X3, X4, X1)
-mi = calc_condmi(X3, X4, X2)
+
+g = Graph()
+g.add_variable("L1", None)
+g.add_variable("L2", "L1")
+g.add_variable("X1", "L1")
+g.add_variable("X2", "L1")
+g.add_variable("X3", "L1")
+g.add_variable("X4", "L2")
+g.add_variable("X5", "L2")
+g.add_variable("X6", "L2")
+g.calc_condmi("X1", "X2", "X4")
+g.calc_condmi("X1", "X4", "X2")
 #import IPython; IPython.embed(); exit(1)
 
